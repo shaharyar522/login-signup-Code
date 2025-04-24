486693d2022d894c5932316335db711a0e7391fc
shaharyar522 <shaharyar.khan4511@gmail.com>
2025-04-24 19:59:43 +0500

    login and signup simple html and css code

---
+++ login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Login Form</title>
+  <link rel="stylesheet" href="css/login.css">
+</head>
+<body>
+  <div class="container">
+    <div class="form login">
+      <header>Login</header>
+      <form action="#">
+        <input type="text" placeholder="Enter your email">
+        <input type="password" placeholder="Enter your password">
+        <a href="#">Forgot password?</a>
+        <input type="submit" class="button" value="Login">
+      </form>
+      <div class="signup">
+        <span>Don't have an account? <a href="signup.html">Signup</a></span>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
---
+++ signup.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Signup Form</title>
+  <link rel="stylesheet" href="css/signup.css">
+</head>
+<body>
+  <div class="container">
+    <div class="form registration">
+      <header>Signup</header>
+      <form action="#">
+        <input type="text" placeholder="Enter your email">
+        <input type="password" placeholder="Create a password">
+        <input type="password" placeholder="Confirm your password">
+        <input type="submit" class="button" value="Signup">
+      </form>
+      <div class="signup">
+        <span>Already have an account? <a href="login.html">Login</a></span>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
